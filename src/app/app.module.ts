a0961566da3f6c528d8bd5f3f9d99a9d34539978
haseeAmarathunga <haseeamarathunga@gmail.com>
2020-01-27 18:10:44 +0530

    Update Passport Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -8,14 +7,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { OCR } from '@ionic-native/ocr/ngx';
+import {Camera} from '@ionic-native/camera/ngx';
+import { CameraPreview } from '@ionic-native/camera-preview/ngx';
+import { Crop } from '@ionic-native/crop/ngx';
+import {ImageCropperModule} from 'ngx-image-cropper';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule, MatProgressBarModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule, ImageCropperModule, HttpClientModule, MatFormFieldModule, ReactiveFormsModule, MatProgressBarModule],
   providers: [
     StatusBar,
     SplashScreen,
+      Camera,
+      Crop,
+      CameraPreview,
+    OCR,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+
+import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import {ImageCropperModule} from 'ngx-image-cropper';
+import {MatCardModule, MatFormFieldModule, MatInputModule, MatProgressBarModule} from '@angular/material';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        HomePageRoutingModule,
+        ImageCropperModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+        MatProgressBarModule
+    ],
   declarations: [HomePage]
 })
 export class HomePageModule {}
